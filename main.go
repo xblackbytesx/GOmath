62fc12636ac58474e43c8a5fd7604bdaf3a75747
Fabio Bier <fabio@proxymail.nl>
2023-02-15 12:03:34 +0100

    Added basic timer support to set a custom timeout for added difficulty.

--- main.go
+++ main.go
@@ -1,3 +1,91 @@
+// package main
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"math/rand"
+// 	"net/http"
+// )
+
+// var currentQuestion *Question
+
+// type Question struct {
+// 	Num1      int    `json:"num1"`
+// 	Num2      int    `json:"num2"`
+// 	Operation string `json:"operation"`
+// 	Options   []int  `json:"options"`
+// 	Answer    int    `json:"answer"`
+// }
+
+// func (q *Question) generateOptions() {
+// 	q.Options = make([]int, 4)
+// 	q.Options[0] = q.Answer
+// 	for i := 1; i < 4; i++ {
+// 		option := rand.Intn(q.Answer*2 + 1)
+// 		if option == q.Answer {
+// 			i--
+// 		} else {
+// 			q.Options[i] = option
+// 		}
+// 	}
+// 	rand.Shuffle(len(q.Options), func(i, j int) {
+// 		q.Options[i], q.Options[j] = q.Options[j], q.Options[i]
+// 	})
+// }
+
+// func generateQuestion() *Question {
+// 	num1 := rand.Intn(100)
+// 	num2 := rand.Intn(100)
+// 	operation := "+"
+// 	answer := num1 + num2
+// 	question := &Question{
+// 		Num1:      num1,
+// 		Num2:      num2,
+// 		Operation: operation,
+// 		Answer:    answer,
+// 	}
+// 	question.generateOptions()
+
+// 	return question
+// }
+
+// func questionHandler(w http.ResponseWriter, r *http.Request) {
+// 	if currentQuestion == nil {
+// 		currentQuestion = generateQuestion()
+// 	}
+// 	json.NewEncoder(w).Encode(currentQuestion)
+// }
+
+// func checkAnswerHandler(w http.ResponseWriter, r *http.Request) {
+// 	defer r.Body.Close()
+// 	var request struct {
+// 		Option int `json:"option"`
+// 	}
+// 	json.NewDecoder(r.Body).Decode(&request)
+
+// 	var message string
+// 	if currentQuestion.Options[request.Option] == currentQuestion.Answer {
+// 		message = "Correct!"
+// 	} else {
+// 		message = fmt.Sprintf("Incorrect, the answer was %d", currentQuestion.Answer)
+// 	}
+
+// 	// generate a new question for the next round
+// 	currentQuestion = generateQuestion()
+
+// 	json.NewEncoder(w).Encode(map[string]string{
+// 		"message": message,
+// 	})
+// }
+
+// func main() {
+// 	http.Handle("/", http.FileServer(http.Dir("./dist")))
+// 	http.HandleFunc("/question", questionHandler)
+// 	http.HandleFunc("/check", checkAnswerHandler)
+// 	http.ListenAndServe(":8080", nil)
+// }
+
+
 package main
 
 import (
@@ -5,6 +93,7 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"time"
 )
 
 var currentQuestion *Question
@@ -15,6 +104,7 @@ type Question struct {
 	Operation string `json:"operation"`
 	Options   []int  `json:"options"`
 	Answer    int    `json:"answer"`
+	Time      int    `json:"time"`
 }
 
 func (q *Question) generateOptions() {
@@ -43,8 +133,20 @@ func generateQuestion() *Question {
 		Num2:      num2,
 		Operation: operation,
 		Answer:    answer,
+		Time:      30, // set timer to 30 seconds
 	}
 	question.generateOptions()
+
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			question.Time--
+			if question.Time == 0 {
+				currentQuestion = generateQuestion()
+				return
+			}
+		}
+	}()
 
 	return question
 }
--- static/js/mathgame.js
+++ static/js/mathgame.js
@@ -2,14 +2,17 @@
 let questionSlot;
 let optionsSlot;
 let resultSlot;
+let timerSlot;
 let defaultResultTemplate;
 let resultTemplate;
+let timerId;
 
 document.addEventListener("DOMContentLoaded", function () {
   // Assign values to global variables
   questionSlot = document.querySelector('[data-content="question"]');
   optionsSlot = document.querySelector('[data-content="options"]');
   resultSlot = document.querySelector('[data-content="result"]');
+  timerSlot = document.querySelector('[data-content="timer"]');
 
   defaultResultTemplate = `<img src="./static/img/thinking.svg" alt="" />`;
   resultTemplate = defaultResultTemplate;
@@ -26,9 +29,15 @@ async function getQuestion() {
     options += `<button id="option${i}" onclick="checkAnswer(${i})">${question.options[i]}</button>`;
   }
   optionsSlot.innerHTML = options;
+
+  // Start the timer
+  startTimer(30);
 }
 
 async function checkAnswer(index) {
+  // Clear the timer
+  clearTimer();
+
   const response = await fetch('/check', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -42,6 +51,26 @@ async function checkAnswer(index) {
   resultSlot.innerHTML = resultTemplate;
   optionsSlot.innerHTML = '';
   setTimeout(getQuestion, 3000);
+}
+
+function startTimer(duration) {
+  let timer = duration;
+  timerId = setInterval(function () {
+    let minutes = Math.floor(timer / 60);
+    let seconds = timer % 60;
+    timerSlot.textContent = `Time remaining: ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+
+    if (--timer < 0) {
+      // Clear the timer and get the next question
+      clearTimer();
+      getQuestion();
+    }
+  }, 1000);
+}
+
+function clearTimer() {
+  clearInterval(timerId);
+  timerSlot.textContent = '';
 }
 
 getQuestion();
