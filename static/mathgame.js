d8c2fb5c93204e8c7f17f4ca1fd5611cf5b0feb5
Fabio Bier <fabio@proxymail.nl>
2023-02-15 01:58:50 +0100

    Changed the binds from ID's to data attributes. Slight styling improvements.

--- static/mathgame.js
+++ static/mathgame.js
@@ -1,14 +1,14 @@
 async function getQuestion() {
   const response = await fetch('/question');
   const question = await response.json();
-  document.querySelector('#result').innerHTML = "";
-  document.querySelector('#question').innerHTML = `What is ${question.num1} ${question.operation} ${question.num2}?`;
+  document.querySelector('[data-content="result"]').innerHTML = "";
+  document.querySelector('[data-content="question"]').innerHTML = `What is ${question.num1} ${question.operation} ${question.num2}?`;
 
   let options = '';
   for (let i = 0; i < question.options.length; i++) {
     options += `<button id="option${i}" onclick="checkAnswer(${i})">${question.options[i]}</button>`;
   }
-  document.querySelector('#options').innerHTML = options;
+  document.querySelector('[data-content="options"]').innerHTML = options;
 }
 
 async function checkAnswer(index) {
@@ -18,8 +18,8 @@ async function checkAnswer(index) {
     body: JSON.stringify({ option: index }),
   });
   const result = await response.json();
-  document.querySelector('#result').innerHTML = result.message;
-  document.querySelector('#options').innerHTML = '';
+  document.querySelector('[data-content="result"]').innerHTML = result.message;
+  document.querySelector('[data-content="options"]').innerHTML = '';
   setTimeout(getQuestion, 3000);
 }
 
